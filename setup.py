a7175798e54310c679e1e17218fb32dff891a550
Sean Richards <Krytic@users.noreply.github.com>
2023-03-06 16:41:23 +1300

    bump version number to v1.0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except FileNotFoundError:
 
 setup(name='glisten',
       license                        = 'MIT License',
-      version                        = 'v1.0.0',
+      version                        = 'v1.0.2',
       description                    = description,
       long_description               = long_description,
       long_description_content_type  = 'text/markdown',
